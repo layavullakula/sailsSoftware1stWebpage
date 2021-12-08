29b9729f85c288487f01bf2b2a036923be9d699e
layavullakula <layavullakula@gmail.com>
2021-12-08 17:17:28 +0530

    second commit

---
+++ javaScript/guessMyNumber/game.js
@@ -0,0 +1,48 @@
+let generatedNumber
+let guessedNumber
+
+function generateNumber(){
+    generatedNumber = Math.trunc(Math.random()*100)+1
+    cover.textContent = "Number generated, Now input the number to check" 
+    generateBtn.classList.add("ndis")
+    checkBtn.classList.remove("ndis")
+    guessInput.classList.remove("ndis")
+}
+
+function generateNumbe(){
+    guessedNumber = guessInput.value
+    console.log(generatedNumber, score.textContent)
+    if (generatedNumber < guessedNumber){
+        outpuText.textContent = "Too High"
+        score.textContent = parseInt(score.textContent)> 0 ?  parseInt(score.textContent) - 1 : gameEnded()
+        wholeBg.classList.remove("wrg2")
+        wholeBg.classList.add("wrg1")
+    }else if (generatedNumber > guessedNumber){
+        outpuText.textContent = "Too Low"
+        score.textContent = parseInt(score.textContent)> 0 ?  parseInt(score.textContent) - 1 : gameEnded()
+        wholeBg.classList.remove("wrg1")
+        wholeBg.classList.add("wrg2")
+    }else{
+        outpuText.textContent = "Right"
+        maxScore.textContent = parseInt(maxScore.textContent)<parseInt(score.textContent) ? score.textContent : maxScore.textContent
+        wholeBg.classList.remove("wrg2", "wrg1")
+        wholeBg.classList.add("crt")
+    }
+}
+
+
+
+function restartGame(){
+    console.log("came")
+    wholeBg.classList.remove("wrg1", "wrg2", "crt")
+    cover.textContent = "Firstly, click on generate Button to generate a number"
+    score.textContent = 20
+    generateBtn.classList.remove("ndis")
+    checkBtn.classList.add("ndis")
+    guessInput.classList.add("ndis")
+    outpuText.textContent = ""
+}
+
+function gameEnded(){
+    alert("Limit Exceeded Click on Restart Button")
+}
